262d1c6aa92daf7d3c291138c4d6653dd10f710e
Filip Furmanek <ffurmanek+github@crefeld.org>
2020-03-19 21:08:42 -0400

    Add files from `flask_app`
    The first step of gluing these two projects together is copying the
    relevent files from the other project into this repo. The file contents
    are unmodified, although some relative locations have changed.
    
    The main `user_profiles_ex01.py` file and the `jinja_templates/` folder
    have both been moved in to the `app/` subfolder, so to follow the layout
    of this project.
    
    The `user_profiles_ex01.json` file remains in the root folder. This is
    where the hardcoded path expects it, so the code can remain unchanged.
    Plus it's adjacent to the app.db file, so it feels right.
    
    This means that the profiles and messages portion of flask_app can now
    be launched from this project using this command:
    
    ```sh
    FLASK_APP=app/user_profiles_ex01.py flask run --host "::1"
    ```
    
    Finally, it is important to note that all of the files in this commit
    are Leo McGuire's work. They have been copied from the latest commit on
    the master branch: 67a38f5916cdac3844b291c0f5295338a7545655

---
+++ app/user_profiles_ex01.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, render_template, abort, redirect, url_for
+import json
+import datetime
+
+app = Flask(__name__, template_folder='jinja_templates')
+
+def load_json():
+	global user_profiles
+	with open('user_profiles_ex01.json') as f:
+		user_profiles = json.load(f)
+
+def save_profiles():
+	with open('user_profiles_ex01.json', 'w') as f:
+		json.dump(user_profiles, f, indent=2)
+
+@app.before_request
+def before_request():
+	load_json()
+
+@app.route('/user')
+def user_list():
+	template_context = dict(users=user_profiles, get_latest_message=get_latest_message)
+	return render_template('user_list_template.html', **template_context)
+
+@app.route('/user/<username>')
+def user_profile(username):
+	try:
+		user = user_profiles[username]
+		return render_template('user_profile_template.html', username=username, user=user)
+	except KeyError:
+		abort(404, f"User {username} not found")
+
+@app.route('/user/<username>/color', methods=['GET','POST'])
+def user_color(username):
+	try:
+		user = user_profiles[username]
+		try:
+			if request.method == 'POST':
+				body = request.get_data(as_text=True)
+				user['color'] = body
+				user['mod_time'] = datetime.datetime.utcnow().isoformat()
+				save_profiles()
+			return user['color']
+		except KeyError:
+			abort(404, f"User {username} does not have a favorite color")
+	except KeyError:
+		abort(404, f"User {username} not found")
+
+@app.route('/user/<username>/mod_time')
+def color_mod_time(username):
+	try:
+		user = user_profiles[username]
+		try:
+			return user['mod_time']
+		except KeyError:
+			abort(404, f"User {username} does not have a mod_time")
+	except KeyError:
+		abort(404, f"User {username} not found")
+
+@app.route('/user/<username>/messages', methods=['GET', 'POST'])
+def user_messages(username):
+	if  request.method == 'POST':
+		post_message(username, request.get_data(as_text=True))
+	try:
+		return user_profiles[username]['messages']
+	except KeyError:
+		abort(404, f"User {username} has no messages")
+
+@app.route('/user/<username>/messages/latest')
+def latest_message(username):
+	timestamp = get_latest_message(username)
+	return redirect(url_for(f'message', username=username, timestamp=timestamp), code=301)
+
+@app.route('/user/<username>/messages/<timestamp>', methods=['GET', 'PUT'])
+def message(username, timestamp):
+	try:
+		if request.method == 'PUT':
+			put_message(username, timestamp, request.get_data(as_text=True))
+		return f"<p>{user_profiles[username]['messages'][timestamp]}</p> <p>{timestamp}</p>"
+	except KeyError:
+		abort(404, f"Message not found")
+
+def get_latest_message(username):
+	return max(user_profiles[username]['messages'].keys())
+
+def post_message(username, body):
+	time = datetime.datetime.utcnow().isoformat()
+	try:
+		user_profiles[username]['messages'][time] = body
+	except KeyError:
+		user_profiles[username]['messages'] = {time: body}
+	save_profiles()
+
+def put_message(username, timestamp, body):
+	try:
+		datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f").isoformat()
+	except ValueError:
+		abort(400, 'Invalid timestamp format')
+	user_profiles[username]['messages'][timestamp] = body
+	save_profiles()
+
+@app.route('/make_coffee/')
+def make_coffee():
+	abort(418)
+
+if __name__ == '__main__':
+	app.run(debug=True)
