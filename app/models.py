e39888a4e9f868306e28337b3d1500af54451b5f
Leo <lmcguire@crefeld.org>
2020-04-30 14:07:15 -0400

    Design initial db structure
    Add Student, Teacher, Exhibitino, Project, and Stage classes to models.py, including variables and relationships.
    
    Created in a meeting with filip-at-crefeld and orangevelocity.
    
    Currently untested, with docstrings and __repr__s to come in later commits.

--- app/models.py
+++ app/models.py
@@ -1,24 +1,45 @@
 from datetime import datetime
 from app import db
 
-
-class Person(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    color = db.Column(db.String(64), index=True)
-    mod_time = db.Column(db.DateTime, index=True)
-    messages = db.relationship('Message', backref='author', lazy='dynamic')
+    student_name = db.Column(db.String(64), index=True)
+    projects = db.relationship('Project', backref='student', lazy='dynamic')
+    advisor_id = db.Column(db.Integer, db.ForeignKey('advisor.id'))
 
     def __repr__(self):
-        return '<Person {}>'.format(self.username)
+        pass
 
-
-class Message(db.Model):
+class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    teacher_name = db.Column(db.String(64), index=True)
+    advisees = db.relationship('Student', backref='advisor', lazy='dynamic')
 
     def __repr__(self):
-        return '<Message {}>'.format(self.body)
+        pass
+
+class Exhibition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exhibition_name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        pass
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    title = db.Column(db.String(64), index=True) # title assigned by student
+    stage = db.relationship('Stage', backref='project', lazy='dynamic')
+
+    def __repr__(self):
+        pass
+
+class Stage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    timestamp = db.Column(db.DateTime, index=True)
+    signer_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
+    checked = db.Column(db.Boolean, index=True)
+
+    def __repr__(self):
+        pass
