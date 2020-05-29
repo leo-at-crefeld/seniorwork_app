9d5609c4b26a5f600a00120a22caa2c01293dc5e
Leo <lmcguire@crefeld.org>
2020-05-29 15:04:43 -0400

    Add create_teacher method
    Creates a teacher and commits it to db, takes a string as name

--- app/db_manager.py
+++ app/db_manager.py
@@ -1,2 +1,12 @@
 from app import db
 from app.models import Student, Teacher, Exhibition, Project, Stage
+
+def create_teacher(name):
+	"""
+	Creates a teacher and commits it to db
+	Takes a string as name
+	"""
+	t = Teacher()
+	t.teacher_name = name
+	db.session.add(t)
+	db.session.commit()
