595da481563c1fe9447a86d777018892c2beb66c
Leo <lmcguire@crefeld.org>
2020-05-29 15:16:52 -0400

    Add create_student method
    Creates a student and relates it to a teacher via advisor_name. Takes two strings as name and advisor name.
    
    No builtin error checking. Will return sqlalchemy.org.exc.NoResultFound if teacher not found by username. Anything that uses this method should take that into account.

--- app/db_manager.py
+++ app/db_manager.py
@@ -10,3 +10,13 @@ def create_teacher(name):
 	t.teacher_name = name
 	db.session.add(t)
 	db.session.commit()
+
+def create_student(name, advisor_name):
+	"""
+	Creates a student and relates it to a teacher via advisor_name
+	Takes two strings as name and advisor_name
+	"""
+	s = Student()
+	s.student_name = name
+	# sqlalchemy.orm.exc.NoResultFound error will be thrown if teacher not found
+	s.advisor_id = Teacher.query.filter_by(teacher_name=teacher_name).one()
