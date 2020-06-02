f49a1c508be66a19a20471b54af67f16c43174f9
Leo <lmcguire@crefeld.org>
2020-06-02 08:29:17 -0400

    Add create_exhibition method
    Creates an exhibition and template project that can be used to create copy projects.

--- app/db_manager.py
+++ app/db_manager.py
@@ -20,3 +20,23 @@ def create_student(name, advisor_name):
 	s.student_name = name
 	# sqlalchemy.orm.exc.NoResultFound error will be thrown if teacher not found
 	s.advisor_id = Teacher.query.filter_by(teacher_name=teacher_name).one()
+
+def create_exhibition(*args):
+	"""
+	Creates an exhibition and template with an arbitrary number of stages
+	Takes first as a string for exhibition_name,
+	the rest of args is strings of the names of stages (in order)
+	"""
+	e = Exhibition()
+	e.exhibition_name = args[0]
+
+	template = Project()
+	template.exhibition_id = exhibition.id
+	template.title = "{}_template".format(e.exhibition_name)
+
+	stages = args[1:]
+	for i in range(len(stages)):
+		s = Stage()
+		s.project_id = template.id
+		s.name = stages[i]
+		s.order = i
