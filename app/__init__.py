d03453f094a387e27c39fe1f0f00a6a57c3989d8
Filip Furmanek <ffurmanek+github@crefeld.org>
2020-06-10 13:19:54 -0400

    Redo last migration using batch mode
    SQLite doesn't support dropping columns, or the ALTER TABLE commands.
    So the most recent migration wouldn't work for us. However, there's a
    "batch mode" that accomplishes things the long way: it creates a table
    with the new schema, copies all the data over, drops the old table, and
    then renames the new table to replace the old one.
    
    If you've got a db that attempted the 2b1421e46110 migration, and you
    want to preserve that data, you probably need to edit it manually.
    If you don't need that data, then just delete the app.db and run "flask
    db upgrade" to generate one from scratch. Everyone else, you won't even
    know that there was a hiccup, unless you find this commit message. ;)
    
    - https://alembic.sqlalchemy.org/en/latest/batch.html#batch-mode-with-autogenerate
    - https://github.com/sqlalchemy/alembic/issues/21
    - https://code.i-harness.com/en/q/1cfc76e
    
    There might have also been complications from the circular reference
    between Project and Exhibition.
    
    - https://docs.sqlalchemy.org/en/13/dialects/sqlite.html#foreign-key-support

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,6 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 from app import routes, models
